38384dc03d03599f23228c073e99bc355ac5703d
Marion <mariondoubeck@yahoo.fr>
2024-02-27 10:48:19 +0100

    get all products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): JsonResponse
+    #[Route('/api/products', name: 'api_products', methods:['GET'])]
+    public function getAllProducts(
+        ProductRepository $productRepository,
+        SerializerInterface $serializerInterface
+    ): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $productList = $productRepository->findAll();
+        $jsonProductList = $serializerInterface->serialize($productList, 'json');
+
+        return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
     }
 }
