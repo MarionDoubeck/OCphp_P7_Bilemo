bd7259b48d1172d82f8ccb21c5572cc43f34106d
Marion <mariondoubeck@yahoo.fr>
2024-03-01 07:58:30 +0100

    improve style

--- src/EventSubscriber/ExceptionSubscriber.php
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -8,8 +8,20 @@ use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
+/**
+ * Subscribes to kernel.exception events and handles exceptions by returning JSON responses.
+ */
 class ExceptionSubscriber implements EventSubscriberInterface
 {
+
+
+    /**
+     * Handles kernel.exception events.
+     *
+     * @param ExceptionEvent $event The event.
+     *
+     * @return void
+     */
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
@@ -21,18 +33,23 @@ class ExceptionSubscriber implements EventSubscriberInterface
             ];
 
             $event->setResponse(new JsonResponse($data));
-      } else {
+        } else {
             $data = [
                 'status' => 500, // Le status n'existe pas car ce n'est pas une exception HTTP, donc on met 500 par défaut.
                 'message' => $exception->getMessage()
             ];
 
             $event->setResponse(new JsonResponse($data));
-      }
+        }
 
     }//end onKernelException()
 
 
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * @return array The event names to listen to.
+     */
     public static function getSubscribedEvents(): array
     {
         return [
