8ee036e16c8da621637352dba09d9b4145517a97
Marion <mariondoubeck@yahoo.fr>
2024-03-01 08:38:42 +0100

    improve style

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,4 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class                            => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class                              => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                        => ['all' => true],
-];
+       ];
